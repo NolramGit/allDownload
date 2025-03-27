80282e4be04e3d97b3d95de062b59a8b27fc23f4
NolramGit <nolrammado@gmail.com>
2025-03-27 16:56:05 -0500

    primera prueba

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { execFile } = require('child_process');
+const path = require('path');
+
+const app = express();
+const puerto = 3000;
+
+// Ruta que ejecutará el archivo de texto
+app.get('/', (req, res) => {
+  const archivo = path.join(__dirname, 'mensaje.txt');
+
+  execFile('cat', [archivo], (error, stdout, stderr) => {
+    if (error) {
+      return res.status(500).send(`Error al ejecutar el archivo: ${error.message}`);
+    }
+    if (stderr) {
+      return res.status(500).send(`Error en el archivo: ${stderr}`);
+    }
+
+    res.send(`Contenido del archivo: <pre>${stdout}</pre>`);
+  });
+});
+
+// Iniciar el servidor
+app.listen(puerto, () => {
+  console.log(`Servidor corriendo en http://localhost:${puerto}`);
+});
